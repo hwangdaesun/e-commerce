c1debdc25ba2abd128df2220211302dd048f98b9
hwangdaesun <hds650588@gmail.com>
2025-11-04 00:26:59 +0900

    feat : id가 Null일 경우 ErrorCode 추가

--- src/main/java/com/side/hhplusecommerce/common/exception/ErrorCode.java
+++ src/main/java/com/side/hhplusecommerce/common/exception/ErrorCode.java
@@ -9,9 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // Item
-    INSUFFICIENT_STOCK("ITEM_001", "재고가 부족합니다.", HttpStatus.CONFLICT);
     INSUFFICIENT_STOCK("ITEM_001", "재고가 부족합니다.", HttpStatus.CONFLICT),
     INVALID_STOCK_QUANTITY("ITEM_002", "차감할 수량은 1 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ORDER_ITEM_ORDER_ID("ORDER_003", "orderId는 필수입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_ORDER_ITEM_ITEM_ID("ORDER_004", "itemId는 필수입니다.", HttpStatus.BAD_REQUEST),
 
     private final String code;
     private final String message;
