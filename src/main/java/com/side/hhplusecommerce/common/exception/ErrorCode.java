666f8c36a8c4ef4fc87acc3c1de032c3d6a1e34e
hwangdaesun <hds650588@gmail.com>
2025-11-04 15:03:54 +0900

    feat : 주문 금액과 쿠폰 할인 금액에 대한 예외 클래스 추가

--- src/main/java/com/side/hhplusecommerce/common/exception/ErrorCode.java
+++ src/main/java/com/side/hhplusecommerce/common/exception/ErrorCode.java
@@ -11,6 +11,10 @@ public enum ErrorCode {
     // Item
     INSUFFICIENT_STOCK("ITEM_001", "재고가 부족합니다.", HttpStatus.CONFLICT),
     INVALID_STOCK_QUANTITY("ITEM_002", "차감할 수량은 1 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
+
+    // Order
+    INVALID_ORDER_AMOUNT("ORDER_001", "주문 금액은 0 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
+    INVALID_COUPON_DISCOUNT("ORDER_002", "쿠폰 할인 금액은 0 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
     INVALID_ORDER_ITEM_ORDER_ID("ORDER_003", "orderId는 필수입니다.", HttpStatus.BAD_REQUEST),
     INVALID_ORDER_ITEM_ITEM_ID("ORDER_004", "itemId는 필수입니다.", HttpStatus.BAD_REQUEST),
     INVALID_ORDER_ITEM_QUANTITY("ORDER_005", "수량은 1개 이상이어야 합니다.", HttpStatus.BAD_REQUEST),
