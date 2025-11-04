41d0a9ef6c118b76075345232ab32cc8838b426b
hwangdaesun <hds650588@gmail.com>
2025-11-04 21:17:34 +0900

    fix : 아이템 재고를 Item 엔티티에서 관리하도록 수정

--- src/main/java/com/side/hhplusecommerce/item/domain/Item.java
+++ src/main/java/com/side/hhplusecommerce/item/domain/Item.java
@@ -1,6 +1,7 @@
 package com.side.hhplusecommerce.item.domain;
 
 import com.side.hhplusecommerce.common.BaseEntity;
+import com.side.hhplusecommerce.item.exception.InsufficientStockException;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,12 +10,29 @@ public class Item extends BaseEntity {
     private Long itemId;
     private String name;
     private Integer price;
+    private Integer stock;
 
     @Builder
-    private Item(Long itemId, String name, Integer price) {
+    private Item(Long itemId, String name, Integer price, Integer stock) {
         super();
         this.itemId = itemId;
         this.name = name;
         this.price = price;
+        this.stock = stock;
+    }
+
+    public void decrease(Integer quantity) {
+        if (!hasEnoughQuantity(quantity)) {
+            throw new InsufficientStockException();
+        }
+        this.stock -= quantity;
+    }
+
+    public void increase(Integer quantity) {
+        this.stock += quantity;
+    }
+
+    public boolean hasEnoughQuantity(Integer quantity) {
+        return this.stock >= quantity;
     }
 }
--- src/test/java/com/side/hhplusecommerce/item/domain/ItemTest.java
+++ src/test/java/com/side/hhplusecommerce/item/domain/ItemTest.java
@@ -7,35 +7,44 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-class ItemStockTest {
+class ItemTest {
 
     @Test
     @DisplayName("재고가 부족할 경우 재고 차감을 하지 않고 예외를 발생시킨다")
     void decrease_fail_insufficient_stock() {
         // given
-        ItemStock itemStock = ItemStock.of(1L, 5);
+        Item item = Item.builder()
+                .itemId(1L)
+                .name("테스트 상품")
+                .price(10000)
+                .stock(5)
+                .build();
 
         // when & then
-        assertThatThrownBy(() -> itemStock.decrease(10))
+        assertThatThrownBy(() -> item.decrease(10))
                 .isInstanceOf(InsufficientStockException.class)
                 .hasMessage(ErrorCode.INSUFFICIENT_STOCK.getMessage());
 
         // 재고가 차감되지 않았는지 확인
-        assertThat(itemStock.getStock()).isEqualTo(5);
+        assertThat(item.getStock()).isEqualTo(5);
     }
-
 
     @Test
     @DisplayName("재고를 정상적으로 증가시킨다")
     void increase_success() {
         // given
-        ItemStock itemStock = ItemStock.of(1L, 10);
+        Item item = Item.builder()
+                .itemId(1L)
+                .name("테스트 상품")
+                .price(10000)
+                .stock(10)
+                .build();
 
         // when
-        itemStock.increase(5);
+        item.increase(5);
 
         // then
-        assertThat(itemStock.getStock()).isEqualTo(15);
+        assertThat(item.getStock()).isEqualTo(15);
     }
 
 }
