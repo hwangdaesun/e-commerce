3b910c9195a72e3d4a10eba50f2cd93733990edd
hwangdaesun <hds650588@gmail.com>
2025-11-05 00:06:07 +0900

    feat : User 엔티티 구현

---
+++ src/main/java/com/side/hhplusecommerce/user/domain/User.java
@@ -0,0 +1,24 @@
+package com.side.hhplusecommerce.user.domain;
+
+import com.side.hhplusecommerce.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class User extends BaseEntity {
+    private Long userId;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private User(Long userId) {
+        super();
+        this.userId = userId;
+    }
+
+    public static User create(Long userId) {
+        return User.builder()
+                .userId(userId)
+                .build();
+    }
+
+}
