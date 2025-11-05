9a86fa97a98feb32f7780052e654590144eb76dc
hwangdaesun <hds650588@gmail.com>
2025-11-05 10:57:10 +0900

    feat : 동시성을 고려하여 ConcurrentHashMap을 사용하도록 변경

--- src/main/java/com/side/hhplusecommerce/item/repository/InMemoryItemRepository.java
+++ src/main/java/com/side/hhplusecommerce/item/repository/InMemoryItemRepository.java
@@ -2,14 +2,13 @@ package com.side.hhplusecommerce.item.repository;
 
 import com.side.hhplusecommerce.item.domain.Item;
 import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryItemRepository implements ItemRepository {
-    private final Map<Long, Item> store = new HashMap<>();
+    private final Map<Long, Item> store = new ConcurrentHashMap<>();
 
     @Override
     public Optional<Item> findById(Long itemId) {
