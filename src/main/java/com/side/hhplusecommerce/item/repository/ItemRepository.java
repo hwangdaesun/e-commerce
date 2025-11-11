59fd210f8bdf24f17aae5579d39a9cd03a9ea810
hwangdaesun <hds650588@gmail.com>
2025-11-11 20:53:57 +0900

    feat : 공유락 사용해 조회하는 쿼리로 수정
    - ItemStockServiceTest 통과하도록 수

--- src/main/java/com/side/hhplusecommerce/item/repository/ItemRepository.java
+++ src/main/java/com/side/hhplusecommerce/item/repository/ItemRepository.java
@@ -1,8 +1,10 @@
 package com.side.hhplusecommerce.item.repository;
 
 import com.side.hhplusecommerce.item.domain.Item;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,4 +21,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("SELECT i FROM Item i WHERE i.itemId IN :itemIds")
     List<Item> findAllByItemIdIn(@Param("itemIds") List<Long> itemIds);
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("SELECT i FROM Item i WHERE i.itemId = :itemId")
+    Optional<Item> findByIdWithOptimisticLock(@Param("itemId") Long itemId);
 }
